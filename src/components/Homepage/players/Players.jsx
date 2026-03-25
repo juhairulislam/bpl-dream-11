0d35158e7521d72803bc8da472bcefddf5437627
juhairulislam <juhairulislam2018@gmail.com>
2026-03-25 21:05:08 +0600

    state management and toggle functionaly

--- src/components/Homepage/players/Players.jsx
+++ src/components/Homepage/players/Players.jsx
@@ -1,17 +1,33 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import AvailablePlayers from './AvailablePlayers/AvailablePlayers';
+import SelectedPlayers from './SelectedPlayers/SelectedPlayers';
 
-const Players = ({playersPromise}) => {
+const Players = ({ playersPromise }) => {
 
-console.log(playersPromise) ;
-const players = use(playersPromise) ;
-// console.log(data)
+    console.log(playersPromise);
+    const players = use(playersPromise);
+    // console.log(data)
+
+
+    const [selectedType, setSelectedType] = useState('Available');
+    // console.log(selectedType)
 
     return (
-        <div className='container mx-auto'>
+        <div className='container w-[90%] mx-auto my-[60px]'>
 
-            <AvailablePlayers players={players}></AvailablePlayers>
-            
+            <div className='flex justify-between gap-2'>
+               { selectedType==='Available' ? <p className='font-bold text-2xl'>Available Players</p> : <p className='font-bold text-2xl'>Selected Players (2/6)</p>}
+
+                <div>
+                    <button onClick={()=> setSelectedType('Available')} className={`btn ${selectedType === "Available" ? "bg-[#E7FE29]" :'btn'} rounded-r-none`}>Available</button>
+                    <button onClick={() => setSelectedType('Selected')} className={` btn ${selectedType === "Selected" ? "bg-[#E7FE29]" :'btn' }  rounded-l-none`}>Selected</button>
+                </div>
+
+            </div>
+
+
+           {selectedType==='Available' ? <AvailablePlayers players={players}></AvailablePlayers>: <SelectedPlayers></SelectedPlayers>}
+
         </div>
     );
 };
---
+++ src/components/Homepage/players/SelectedPlayers/SelectedPlayers.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SelectedPlayers = () => {
+    return (
+        <div>
+            This is selected players
+            
+        </div>
+    );
+};
+
+export default SelectedPlayers;
