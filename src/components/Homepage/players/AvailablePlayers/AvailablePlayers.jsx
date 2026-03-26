00b024a5f5bcace9fdf5ee69acd25778902aff3f
juhairulislam <juhairulislam2018@gmail.com>
2026-03-26 14:52:10 +0600

    more console delet

--- src/components/Homepage/players/AvailablePlayers/AvailablePlayers.jsx
+++ src/components/Homepage/players/AvailablePlayers/AvailablePlayers.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import Card from '../../../UI/Card'
 
 const AvailablePlayers = ({ players,setCoin,coin,setSelectedPlayers, selectedPlayers }) => {
-    // console.log(players)
     return (
         <div className='mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
 
